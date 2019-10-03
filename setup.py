49a8f35605fb1576f055e0a51937bdfbcfd7b045
Sander Schaminée <8972335+GeoSander@users.noreply.github.com>
2019-10-03 17:57:05 +0200

    Added geocom.ch url

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         long_description=read('README.rst'),
         author='Geocom Informatik AG / VertiGIS, Burgdorf, Switzerland',
         author_email='github@geocom.ch',
+        url='https://geocom.ch/',
         project_urls={
             'Source': 'https://github.com/geocom-gis/gntools',
             'Documentation': 'https://gntools.readthedocs.io/'
