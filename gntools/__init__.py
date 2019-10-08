53b1b6fd5d24206f2f456afa673c0cde0be47ce0
Sander Schaminee <8972335+GeoSander@users.noreply.github.com>
2019-10-08 15:23:32 +0200

    Moved modules in core subpackage into gntools root. Updated __init__.py in root.

--- gntools/__init__.py
+++ gntools/__init__.py
@@ -17,6 +17,7 @@
 """
 This is the documentation for the **gntools** package for **Python 2.7** (only).
 The package provides a toolset for the creation of GEONIS menu or form scripts.
+
 GEONIS is a powerful GIS solution by Geocom Informatik AG (Switzerland), built on top of ArcGIS technology.
 Although this package does not require a license to run and works fully independent of the GEONIS software,
 please note that GEONIS itself *does* require a license.
@@ -27,4 +28,12 @@ allowing the community to freely use it, improve it and possibly add new feature
 Several functions in this package require Esri's ``arcpy`` Python library, which does not make this a *free* package.
 However, users who have already installed and authorized ArcGIS Desktop (ArcMap, ArcCatalog etc.) should be able to work
 with this package without any problems.
+
+One of the most notable classes is the :class:`~gntools.protocol.ProtocolLogger` class,
+which can be used to write messages and features to a GEONIS XML log file (or *Protocol*).
+Users can read this file back in with GEONIS and click on the listed features,
+which is useful for inspection and/or validation purposes.
+
+Also of interest might be the :py:mod:`~gntools.params` module. This module contains classes that
+help parse passed-in arguments from GEONIS menu or form scripts in a standardized and user-friendly manner.
 """
